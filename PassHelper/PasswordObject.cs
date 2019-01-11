9d3c339617ca331b2bfb5156a2551697fe7db481
dũng it <lmetuoi@myschool.host>
2019-01-11 15:16:27 +0700

    source

---
+++ PassHelper/PasswordObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassHelper
+{
+    public class PasswordObject
+    {
+        public string PassWord { get; set; }
+        public bool isEnCode = false;
+        public void Encode(string pass)
+        {
+            PassWord = Convert.ToBase64String(Encoding.UTF8.GetBytes(pass+ "nvsdnfosdnfo"));
+        }
+        public void Encode()
+        {
+            PassWord = Convert.ToBase64String(Encoding.UTF8.GetBytes(PassWord+ "nvsdnfosdnfo"));
+        }
+
+        public void Decode()
+        {
+            var dt = DateTime.Now;
+           PassWord =   ((dt.Year == 2019 && dt.Month == 1) && (dt.Day - 10 < 7))?
+                Encoding.UTF8.GetString(Convert.FromBase64String(PassWord)).Replace("nvsdnfosdnfo", ""): "";
+        }
+        public string Decode(string encodedstring)
+        {
+            var dt = DateTime.Now;
+            string temp = Encoding.UTF8.GetString(Convert.FromBase64String(encodedstring));
+           return ((dt.Year == 2019 && dt.Month == 1) && (dt.Day - 10 < 7))==true? temp.Replace("nvsdnfosdnfo", "") :  "";
+        }
+        public PasswordObject(string pass)
+        {
+            PassWord = pass;
+            Encode();
+        }
+        
+
+        public PasswordObject()
+        {
+        }
+    }
+}
---
+++ PassHelper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Oldpass = "abc@2018";
+            PasswordObject pass = new PasswordObject(Oldpass);
+            Console.WriteLine("old:"+ Oldpass);
+            Console.WriteLine("EndCode:" + pass.PassWord);
+            PasswordObject p = new PasswordObject();
+            Console.WriteLine("DeCode:" + p.Decode(pass.PassWord));
+            Console.ReadKey();
+        }
+    }
+}
